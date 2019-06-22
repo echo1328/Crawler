12672fb7d8e4bb895f4b67522a9f302a7a829133
wenlo <wenlongzhou@aliyun.com>
2019-06-22 18:01:50 +0800

    commit

--- crawler-es/src/test/java/ESTest.java
+++ crawler-es/src/test/java/ESTest.java
@@ -1,23 +1,16 @@
-package com.es.test;
-
 import com.es.pojo.Item;
 import com.es.service.ItemService;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @program: crawler
  * @description: ESTest
  * @author: wenlongzhou
- * @create: 2019-06-22 15:08
+ * @create: 2019-06-22 16:24
  **/
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath:applicationContext.xml")
 public class ESTest {
 
     @Autowired
